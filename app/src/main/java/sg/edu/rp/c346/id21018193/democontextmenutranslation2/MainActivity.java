c84224137dcff75123ca25e967ea895b3575869b
lee zhuo hong <21018193@myrp.edu.sg>
2022-06-01 11:04:26 +0800

    e

--- app/src/main/java/sg/edu/rp/c346/id21018193/democontextmenutranslation2/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/democontextmenutranslation2/MainActivity.java
@@ -26,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-                                    ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
+
 
 
     @Override
